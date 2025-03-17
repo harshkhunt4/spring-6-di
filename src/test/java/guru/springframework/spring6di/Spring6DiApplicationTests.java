104550fa2fc897f41d65cdd7affb6de832f9d825
Harsh <harsh.khunt@einfochips.com>
2025-03-17 21:23:08 +0530

    spring test context

--- src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,13 +1,31 @@
 package guru.springframework.spring6di;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.spring6di.controllers.MyController;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+  @Autowired
+  private ApplicationContext context;
+
+  @Autowired
+  private MyController myController;
+
+  @Test
+  void testAutowiredOfController() {
+    System.out.println(myController.sayHello());
+  }
+
+  @Test
+  void testGetControllerFromCntx() {
+    MyController myController = context.getBean(MyController.class);
+
+    System.out.println(myController.sayHello());
+  }
 
 }
