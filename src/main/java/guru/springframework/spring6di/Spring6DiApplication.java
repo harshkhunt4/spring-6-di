627591deb1724133c7929c2f432e105ad819f716
Harsh <harsh.khunt@einfochips.com>
2025-03-17 20:54:12 +0530

    sprirng context

--- src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
+++ src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
@@ -2,12 +2,22 @@ package guru.springframework.spring6di;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.spring6di.controllers.MyController;
 
 @SpringBootApplication
 public class Spring6DiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Spring6DiApplication.class, args);
-    }
+  public static void main(String[] args) {
+    ApplicationContext run = SpringApplication.run(Spring6DiApplication.class, args);
+
+    MyController controller = run.getBean(MyController.class);
+
+    System.out.println("In Main Method");
+
+    System.out.println(controller.sayHello());
+
+  }
 
 }
